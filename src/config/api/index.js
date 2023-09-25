5d2260bcdf393683cf19c26cba8ce9c12c9e41e2
Bruno Rocha <bruno.rocha@apllos.com>
2023-09-25 10:28:06 -0300

    ultimos ajustes

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const ApiClient = axios.create({
- baseURL: "http://localhost:9000",
- //baseURL: "http://helpdesk.techniconnection.com:9000",
+ //baseURL: "http://localhost:9000",
+ baseURL: "http://helpdesk.techniconnection.com:9000",
 });
 
 export { ApiClient };
