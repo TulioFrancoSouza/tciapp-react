2ed2c3035f8e6fd33abd7e419e6c5fbf406b275f
jose guilherme <guilhermedev2002@gmail.com>
2023-09-28 16:36:53 -0300

    fix: ajustando layout do ModalReport

--- src/components/modal/ModalReport.jsx
+++ src/components/modal/ModalReport.jsx
@@ -6,25 +6,25 @@ const Modal = ({ accept, deny, closeModalReport }) => {
       onClick={closeModalReport}
       className="w-screen h-screen drop-shadow-2xl flex justify-center items-center fixed top-0 right-0 bg-gray-400/80"
     >
-      <div className="em:w-[300px] em:h-[175px] w-[400px] h-[200px] bg-white px-3 py-3 rounded-lg">
+      <div className="w-[250px] md:w-[300px] bg-white px-3 py-3 rounded-lg">
         <div className="flex justify-end">
           <button onClick={closeModalReport}>X</button>
         </div>
         <div>
-          <h2 className="flex justify-center items-center">
+          <h2 className="flex justify-center items-center font-bold text-center">
             Do you want to send the report?
           </h2>
         </div>
         <div className="mt-8 flex justify-around w-full">
           <button
             onClick={accept}
-            className="px-10 py-2 rounded-lg drop-shadow-2xl bg-lime-400"
+            className="px-10 py-2 rounded-lg drop-shadow-2xl bg-lime-400 text-white"
           >
             Yes
           </button>
           <button
             onClick={deny}
-            className="px-10 py-2 rounded-lg drop-shadow-2xl bg-red-400"
+            className="px-10 py-2 rounded-lg drop-shadow-2xl bg-red-400 text-white"
           >
             No
           </button>
