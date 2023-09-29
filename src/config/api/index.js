1cd49ddab6221e5b7ff3e70c0b557170b4217b2c
Bruno Rocha <bruno.rocha@apllos.com>
2023-09-29 11:37:24 -0300

    Merge branch 'main' into fix/layout

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ApiClient = axios.create({
  //baseURL: "http://localhost:9000",
- baseURL: "https://mtl.techniconnection.com:9000",
+ baseURL: "http://mtl.techniconnection.com:9000",
 });
 
 export { ApiClient };
